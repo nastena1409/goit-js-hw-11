56693c7c497f435cc49eb9636cc12d0583f976f5
nastena1409 <nastena1409@gmail.com>
2023-05-20 20:11:23 +0200

    fixed renderGallery

--- src/render-gallery.js
+++ src/render-gallery.js
@@ -12,25 +12,18 @@ function renderGallery(gallery) {
             }) => {
                 return `
         <div class="photo-card">
-        <a class="gallery__link" href="${largeImageURL}">
-          <img class="gallery-image"
-               src="${webformatURL}" 
-               alt="${tags}" 
-               loading="lazy" />
-        </a>  
-          <div class="info">
-            <p class="info-item">
-                <b>Likes</b>${likes}
-            </p>
-            <p class="info-item">
-                <b>Views</b>${views}
-            </p>
-            <p class="info-item">
-                <b>Comments</b>${comments}
-            </p>
-            <p class="info-item">
-                <b>Downloads</b>${downloads}
-            </p>
+            <a class="gallery__link" href="${largeImageURL}">
+                <img class="gallery-image"
+                    src="${webformatURL}" 
+                    alt="${tags}" 
+                    loading="lazy" />
+            </a>  
+            <div class="info">
+                <p class="info-item"><b>Likes</b>${likes}</p>
+                <p class="info-item"><b>Views</b>${views}</p>
+                <p class="info-item"><b>Comments</b>${comments}</p>
+                <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
         </div>`}).join("");
     
 }  
